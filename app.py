d50d4144f740b02e3aff8f1f2145705bc25e3cfe
Your Name <your.email@example.com>
2024-12-30 22:17:01 +0100

    railway

--- app.py
+++ app.py
@@ -125,5 +125,6 @@ def download(username):
         flash(f'Error: {error_msg}', 'error')
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    app.run(debug=True) 
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
